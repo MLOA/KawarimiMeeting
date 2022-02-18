a176a7bfd09c44f7e3a946b859079e1a3d049aaa
pvcresin <pvcresin0730@gmail.com>
2022-02-18 16:55:15 +0900

    Video player popup

--- src/client/popup.js
+++ src/client/popup.js
@@ -1,3 +1,14 @@
-const p = document.querySelector('p');
+// https://qiita.com/mml/items/da6eaf5e906e1d40481d
+function init() {
+  const input = document.getElementById("filename");
+  input.addEventListener(
+    "change",
+     (event) => {
+      const file = event.target.files[0];
+      document.querySelector("video").src = URL.createObjectURL(file);
+    },
+    false
+  );
+}
 
-p.textContent = chrome.extension.getURL("sample.mp4");
+init();
