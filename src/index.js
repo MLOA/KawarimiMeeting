8f676da11df5eea18c274d848a0f1de3238ccbe3
shisoAqron <nooba704@gmail.com>
2022-02-18 21:43:40 +0900

    play and stop button(WIP)

--- src/index.js
+++ src/index.js
@@ -31,6 +31,17 @@ window.onload = () => {
       transitStartRecButton.click();
     } else if (actionType === "end-rec") {
       transitEndRecButton.click();
-    }
+    } else if (actionType === "video-play") {
+      const virtualDevice = Array.from(document.querySelectorAll('.video-option-menu__pop-menu li > a'))
+        .filter((s) => { return s.textContent === "Loop!!!" })[0];
+      // 1回でも表示しないと操作できないぽい
+      document.querySelectorAll('.video-option-menu > button')[0].click()
+      virtualDevice.click();
+    } else if (actionType === "video-stop") {
+      const deviceList = Array.from(document.querySelectorAll('.video-option-menu__pop-menu li > a'));
+      const defaultDevice = deviceList[deviceList.length -2]
+      document.querySelectorAll('.video-option-menu > button')[0].click()
+      defaultDevice.click();
+    } 
   });
 };
--- src/popup.js
+++ src/popup.js
@@ -9,3 +9,15 @@ document.querySelector(".end-rec").addEventListener("click", (ev) => {
     chrome.tabs.sendMessage(tabs[0].id, { actionType: "end-rec" });
   });
 });
+
+document.querySelector(".video-play").addEventListener("click", (ev) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { actionType: "video-play" });
+  });
+});
+
+document.querySelector(".video-stop").addEventListener("click", (ev) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { actionType: "video-stop" });
+  });
+});
