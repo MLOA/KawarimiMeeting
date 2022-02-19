83e27653c45b3e45dcd91767866b28ba0b6c4c5c
pvcresin <pvcresin0730@gmail.com>
2022-02-19 09:11:22 +0900

    Fix sendData

--- src/popup.js
+++ src/popup.js
@@ -10,7 +10,7 @@ const ACTION_TYPE = { START_REC: "start-rec", END_REC: "end-rec" };
 
 const sendData = (actionType) => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { actionType});
+    chrome.tabs.sendMessage(tabs[0].id, { actionType });
   });
 }
 
@@ -22,16 +22,12 @@ recordButton.addEventListener("click", (ev) => {
   isRecording = !isRecording;
 
   if (isRecording) {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      sendData(ACTION_TYPE.START_REC)
-    });
+    sendData(ACTION_TYPE.START_REC);
     indicator.classList.remove("stopping");
     indicator.classList.add("recording");
     preview.style.display = "none";
   } else {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      sendData(ACTION_TYPE.END_REC)
-    });
+    sendData(ACTION_TYPE.END_REC);
     indicator.classList.remove("recording");
     indicator.classList.add("stopping");
     preview.style.display = "initial";
